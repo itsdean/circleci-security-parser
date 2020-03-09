0ff9063eec3401de6a8c4c6ea805e466709f0a67
salidas <mail@salidas.wtf>
2020-03-09 11:17:02 +0000

    Added documentation to IssueHolder

--- lib/issues/IssueHolder.py
+++ lib/issues/IssueHolder.py
@@ -1,16 +1,35 @@
 class IssueHolder:
+    """
+    Simple class just used to retain information on the issues found during this parser's run.
+    """
 
     def __init__(self):
+        """
+        Simple init - creates the instance's list object.
+        """
+
         self.findings_list = []
 
 
     def add(self, issue):
+        """
+        Adds a new issue to the list of issues.
+        """
+
         self.findings_list.append(issue)
 
 
     def get_issues(self):
+        """
+        Returns the current list of issues.
+        """
+
         return self.findings_list
 
 
     def size(self):
+        """
+        Returns the size of the current list of issues.
+        """
+
         return len(self.findings_list)
