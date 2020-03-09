4c819d38831e5c69b2689787acd6756ae0c1fe36
salidas <mail@salidas.wtf>
2020-03-09 13:08:25 +0000

    Moved burrow to lib/parser

---
+++ lib/parser/burrow.py
@@ -0,0 +1,52 @@
+import json
+
+def parse(burrow_file, reporter, output_wrapper):
+    """
+    Goes through burrow tool output and passes each reported issue to Reporter.
+    """
+
+    # Set the "constants now"
+    issue_type = "secrets"
+    tool_name = "burrow"
+
+    # Without 100% confidence we don't want to recommend removing the line. 
+    # We'll leave it up to manual work to triage this.
+    recommendation = "Please identify whether this finding is a true or false positive. Consider adding the file and line to .burrowignore if this is a false positive."
+
+    # Because burrow_file is a File object, we need the filename for outputting reasons. 
+    filename = burrow_file.name # Do I need to do this? How often am I called?
+
+    # Unmarshal the file into a JSON object
+    json_object = json.load(burrow_file)
+
+    # Get the reported issues from the JSON blob.
+    findings = json_object["findings"]
+
+    findings_size = len(findings) # Do I need this either?
+    output_wrapper.add("- " + str(findings_size) + " findings reported by burrow!")
+
+    for issue in findings:
+        
+        title = issue["match"]
+        location = issue["file"]
+
+        description = "A potentially hardcoded secret was identified."
+
+        # Check if the issue output included an affected line
+        # If it did, add the line to the path.
+        if isinstance(
+            issue["line"],
+            int
+        ):
+            location += ":" + str(issue["line"])
+
+        reporter.add(
+            issue_type,
+            tool_name,
+            title,
+            description,
+            location,
+            recommendation,
+            filename,
+            raw_output = issue
+        )
