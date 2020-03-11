445f9765a5bf6bea14cdf5ef17ce374011ed8324
salidas <mail@salidas.wtf>
2020-03-11 10:41:22 +0000

    Moved deduplication step to IssueHolder
    - Added passing of OutputWrapper instance to IssueHolder

--- lib/issues/IssueHolder.py
+++ lib/issues/IssueHolder.py
@@ -1,14 +1,79 @@
+import hashlib
+import json
+
 class IssueHolder:
     """
     Simple class just used to retain information on the issues found during this parser's run.
     """
 
-    def __init__(self):
+    def __init__(self, output_wrapper):
         """
         Simple init - creates the instance's list object.
         """
 
+        self.output_wrapper = output_wrapper
+
+        # self.output_wrapper.set_title("Deduplicating...")
         self.findings_list = []
+
+
+    def create_hash(self, issue):
+        """
+        Creates a unqiue hash using fields from the issue.
+        This hash will be used for deduplication and 
+        """
+
+        description = issue["description"].encode("utf-8")
+        location = issue["location"].encode("utf-8")
+
+        # issue = json.dumps(issue, sort_keys=True, default=str)
+
+        issue_hash = hashlib.sha256(
+            description + b":" + location
+        ).hexdigest()
+
+        # self.output_wrapper.add("Generated hash: " + issue_hash)
+        
+        return issue_hash
+
+
+    def deduplicate(self):
+        """
+        Goes through the list and removes and duplicate issues.
+        In the process, it also creates a unique hash for each issue.
+        """
+
+        self.output_wrapper.set_title("Deduplicating...")
+
+        # Create an empty list that will store calculated hashes.
+        issue_hash_oracle = []
+
+        # Create an empty list that will contain the unique issues.
+        deduplicated_findings = []
+
+        # Iterate though a deep copy of the current issues we have
+        for element in list(self.get_issues()):
+ 
+            # Get the contents of the issue in dictionary format
+            issue = element.get()
+
+            issue_hash = self.create_hash(issue)
+
+            if issue_hash not in issue_hash_oracle:
+
+                issue_hash_oracle.append(issue_hash)
+                deduplicated_findings.append(issue)
+
+            # issue_hash = create_hash()
+
+        # The description and location of each issue is merged together and
+        # hashed - if this hash has not been dealt with (this parsing round)
+        # before then we'll accept it, otherwise ignore it.
+
+        self.output_wrapper.add("- Array size: " + str(self.size()))
+        self.output_wrapper.add("- Array size after deduplication: " + str(len(deduplicated_findings)))
+
+        return deduplicated_findings
 
 
     def add(self, issue):
--- lib/output/Reporter.py
+++ lib/output/Reporter.py
@@ -57,7 +57,7 @@ class Reporter:
 
         # Create the instances we will be calling throughout this class
         self.output_wrapper = OutputWrapper()
-        self.issue_holder = IssueHolder()
+        self.issue_holder = IssueHolder(self.output_wrapper)
 
         self.csv_name = self.prepare_csv_name()
 
@@ -109,52 +109,6 @@ class Reporter:
         )
 
 
-    def deduplicate(self):
-        """
-        Goes through the list of submitted issues and removes any issues that have been reported more than once.
-
-        The description and location of each issue is merged together and hashed - if this hash has not been dealt with (this parsing round) before then we'll accept it, otherwise ignore it.
-        """
-
-        self.output_wrapper.set_title("Deduplicating...")
-
-        # Create an empty list that will contain the unique issues.
-        deduplicated_findings = []
-
-        # Use a secondary list that will only contain issue descriptions as
-        # keys. We'll use hashes of the combination of the description and 
-        # location as existence oracles
-        issue_hash_oracle = []
-
-        # Make a new list object and hard copy all current issues to that
-        # object. Iterate through it.
-        for element in list(self.issue_holder.get_issues()):
-
-            # Get the issue in dictionary format
-            issue = element.get()
-
-            # Generate a hash from fields of the issue. This will be used to uniquely identify that issue
-            issue_hash = hashlib.sha256(
-                # issue["description"].encode("utf-8") + b":" + issue["location"].encode("utf-8")
-                issue["description"].encode("utf-8") + b":" + issue["location"].encode("utf-8")
-            ).hexdigest()
-
-            # Check if the description for the issue's not already in the lookup table list
-            if issue_hash not in issue_hash_oracle:
-
-                # If we've reached this line, then it's a new issue we haven't seen before and we can report it.
-                # Add the description to the oracle list
-                issue_hash_oracle.append(issue_hash)
-
-                # Add the full issue to the new list
-                deduplicated_findings.append(issue)
-
-        self.output_wrapper.add("- Array size: " + str(self.issue_holder.size()))
-        self.output_wrapper.add("- Array size after deduplication: " + str(len(deduplicated_findings)))
-
-        return deduplicated_findings
-
-
     def create_csv_report(self):
         """
         Obtains the current list of issues and prints them to a CSV file.
@@ -170,7 +124,7 @@ class Reporter:
 
             self.output_wrapper.set_title("Generating CSV report...")
 
-            deduplicated_findings = self.deduplicate()
+            deduplicated_findings = self.issue_holder.deduplicate()
 
             with open(self.csv_location, 'w+', newline="\n") as csv_file_object:
                 writer = csv.DictWriter(csv_file_object, fieldnames=get_fieldnames())
