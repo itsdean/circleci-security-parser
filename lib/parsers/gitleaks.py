7fc0b81751c98a1ae18de5b94b811a2a87d6c344
salidas <mail@salidas.wtf>
2020-05-14 11:45:45 +0100

    Made use of gitleaks own JSON report output.

--- lib/parsers/gitleaks.py
+++ lib/parsers/gitleaks.py
@@ -5,27 +5,27 @@ from ..constants import an
 # tmp
 from pprint import pprint
 
-def create_json_object(gitleaks_file, output_wrapper):
-    """
-    Parses the file created by gitleaks and crafts it into a JSON object
-    we can play around and parse.
+# def create_json_object(gitleaks_file, output_wrapper):
+#     """
+#     Parses the file created by gitleaks and crafts it into a JSON object
+#     we can play around and parse.
 
-    By default, gitleaks reports a JSON object for each offense, but does
-    not combine them into a single well-formed JSON blob.
+#     By default, gitleaks reports a JSON object for each offense, but does
+#     not combine them into a single well-formed JSON blob.
 
-    This aims to solve that, returning a Python JSON object as a result.
-    """
+#     This aims to solve that, returning a Python JSON object as a result.
+#     """
 
-    # Create an empty array. This will store the individual JSON objects
-    json_object = []
+#     # Create an empty array. This will store the individual JSON objects
+#     json_object = []
 
-    # gitleaks_file is passed to this point as a file object already, hence
-    # the lack of open().
-    for line in gitleaks_file:
-        tmp_json_object = json.loads(line)
-        json_object.append(tmp_json_object)
+#     # gitleaks_file is passed to this point as a file object already, hence
+#     # the lack of open().
+#     for line in gitleaks_file:
+#         tmp_json_object = json.loads(line)
+#         json_object.append(tmp_json_object)
 
-    return json_object
+#     return json_object
 
 def parse(gitleaks_file, issue_holder, output_wrapper):
     """
@@ -42,7 +42,8 @@ def parse(gitleaks_file, issue_holder, output_wrapper):
     recommendation = "Please identify whether this finding is true or false positive."
 
     output_wrapper.add("- Creating well-formed JSON object for parsing")
-    formatted_file = create_json_object(gitleaks_file, output_wrapper)
+    # formatted_file = create_json_object(gitleaks_file, output_wrapper)
+    formatted_file = json.load(gitleaks_file)
 
     output_wrapper.add("- {} findings reported by gitleaks!".format(len(formatted_file)))
 
