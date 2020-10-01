7a394bee9b25018ea4303af4556511844bdeaf86
itsdean <hey@dean.dev>
2020-10-01 18:26:24 +0100

    gitleaks: added mention of secret in description

--- lib/parsers/gitleaks.py
+++ lib/parsers/gitleaks.py
@@ -21,10 +21,12 @@ def parse(gitleaks_file, issue_holder, logger):
     for issue in gitleaks_issues:
 
         if "key" in issue["tags"]:
-            title = "Potential {} match".format(issue["rule"])
+            title = f"Potential {issue['rule']} match"
             description = "A string matching a key was found in a file. Gitleaks reported it as {}.".format(an(issue["rule"]))
+            description = f"A string matching a key was found in a file; gitleaks reported it as {an(issue['rule'].lower())}.\n\nThe offence can be found below:\n{issue['offender']}"
         else:
             title = issue["rule"]
+            description = f"A potential credential was found in a file. Gitleaks reported it as {an(issue['rule'].lower())}.\n\nThe offence can be found below:\n{issue['offender']}"
 
         location = issue["file"]
         filename = issue["file"]
