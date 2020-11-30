b175eb96fe0ea9db2d18c594ca91f34ce9a47593
itsdean <hey@dean.dev>
2020-11-30 11:07:19 +0000

    gitleaks: added support for grouping issues per file

--- lib/parsers/gitleaks.py
+++ lib/parsers/gitleaks.py
@@ -4,22 +4,22 @@ from ..constants import an
 
 MAX_LINE_LENGTH = 100
 
-def parse(gitleaks_file, issue_holder, logger, metadata):
+ISSUE_TYPE = "secrets"
+TOOL_NAME = "gitleaks"
+
+# Due to the potential risk of it being a real credential, we should always make these stand out.
+SEVERITY = "medium"
+
+RECOMMENDATION = """Please identify whether this finding is valid.
+It is recommended to make use of secrets management tools/functionality such as
+AWS Secrets Manager to retrieve sensitive values or credentials when required, rather than hardcoding contents.
+"""
+
+def parse_individual(gitleaks_issues, issue_holder, logger, metadata):
     """
     Goes through gitleaks --verbose output, combines them into a proper JSON
     object, then passes each reported issue to Reporter.
     """
-
-    issue_type = "secrets"
-    tool_name = "gitleaks"
-
-    # Due to the potential risk of it being a real credential, we should always make these stand out.
-    severity = "medium"
-
-    recommendation = "Please identify whether this finding is valid.\nIt is recommended to make use of secrets management tools/functionality such as "
-    recommendation += "AWS Secrets Manager to retrieve sensitive values or credentials when required, rather than hardcoding contents."
-
-    gitleaks_issues = json.load(gitleaks_file)
 
     for issue in gitleaks_issues:
 
@@ -35,7 +35,6 @@ def parse(gitleaks_file, issue_holder, logger, metadata):
             location += f'/blob/{issue["commit"]}/{issue["file"]}'
             if issue["lineNumber"] > 0:
                 location += f'#L{issue["lineNumber"]}'
-
 
         # Report the issue differently if the rule is for a file pattern
         if "Filename/path" in issue["offender"]:
@@ -69,15 +68,101 @@ def parse(gitleaks_file, issue_holder, logger, metadata):
         custom["file_location"] = issue["file"]
 
         issue_holder.add(
-            issue_type,
-            tool_name,
+            ISSUE_TYPE,
+            TOOL_NAME,
             title,
             description,
             location,
-            recommendation,
-            severity = severity,
+            RECOMMENDATION,
+            severity = SEVERITY,
             raw_output = issue,
             custom = custom
         )
 
     logger.debug(f"> gitleaks: {len(gitleaks_issues)} issues reported\n")
+
+
+def parse_multiple(gitleaks_issues, issue_holder, logger, metadata):
+
+    files = {}
+
+    # Merge issues together if they're from the same file
+    for issue in gitleaks_issues:
+
+        if issue["file"] not in files.keys():
+            files[issue["file"]] = {
+                "offences": [
+                    {
+                        "rule": issue["rule"],
+                        "lineNumber": issue["lineNumber"],
+                        "line": issue["line"],
+                        "offender":  issue["offender"],
+                    }
+                ],
+                "title": f"Potential credentials found at \"{issue['file']}\"",
+                "commit": issue["commit"],
+                "path": issue["file"],
+                "repository_path_known": False,
+                "repository_path": "N/A",
+                "description": f'Potential credentials were found in a file.'
+            }
+        else:
+            files[issue["file"]]["offences"].append(
+                {
+                    "rule": issue["rule"],
+                    "lineNumber": issue["lineNumber"],
+                    "line": issue["line"],
+                    "offender":  issue["offender"]
+                }
+            )
+
+        # Deduce the file path (and tailor the description) if it's not a whole-file rule
+        if "Filename/path" not in issue["offender"]:
+
+            # Get the repository file URL
+            path = metadata.repository_url
+            path += f"/blob/{files[issue['file']]['commit']}/{issue['file']}"
+
+            # Add an atomic line to the description and save the file URL
+            if not files[issue["file"]]["repository_path_known"]:
+                files[issue["file"]]["description"] += "\n\nMatches for the following gitleaks rule(s) were found:"
+                files[issue["file"]]["reepository_path_known"] = True
+                files[issue["file"]]["repository_path"] = path
+
+            # Add an entry in the description for this offence
+            files[issue["file"]]["description"] += f"\n- {issue['rule']} at line {issue['lineNumber']}: "
+            if len(issue["line"]) > MAX_LINE_LENGTH:
+                files[issue["file"]]["description"] += f"{issue['offender']}"
+            else:
+                files[issue["file"]]["description"] += f"{issue['line']}"
+
+        # If it's a whole-file rule, tailor the description
+        else:
+            files[issue["file"]]["description"] += f"\nThe file that triggered this rule was \"{issue['file']}\""
+            files[issue["file"]]["description"] += "\nAs this was a filename/path rule that triggered, please search the repository for matching files and confirm if they are valid."
+
+    for offending_file in files.keys():
+
+        issue_holder.add(
+            ISSUE_TYPE,
+            TOOL_NAME,
+            files[offending_file]["title"],
+            files[offending_file]["description"],
+            files[offending_file]["path"],
+            RECOMMENDATION,
+            severity = SEVERITY,
+            raw_output = files[offending_file],
+            custom = {
+                "file_location": files[offending_file]["path"]
+            }
+        )
+
+    logger.debug(f"> gitleaks: {len(files)} issues reported\n")
+
+def parse(gitleaks_file, issue_holder, logger, metadata):
+    gitleaks_issues = json.load(gitleaks_file)
+
+    if metadata.gitleaks and "individual" in metadata.gitleaks and metadata.gitleaks["individual"]:
+        parse_individual(gitleaks_issues, issue_holder, logger, metadata)
+    else:
+        parse_multiple(gitleaks_issues, issue_holder, logger, metadata)
